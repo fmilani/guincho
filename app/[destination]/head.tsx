908ece55234324d649df76da7cdd2f01168df30e
Felipe Milani <felmilani@gmail.com>
2022-12-30 17:55:19 -0300

    head component for destination page + og

---
+++ app/[destination]/head.tsx
@@ -0,0 +1,26 @@
+export default function DestinationHead({ params: { destination } }: any) {
+  return (
+    <>
+      <title>{`${decodeURI(destination)} | Pelego Guincho`}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width" />
+      <meta property="og:title" content="Pelego Auto Guincho" />
+      <meta
+        property="og:description"
+        content={`Valor do frete para ${decodeURI(destination)}`}
+      />
+      <meta
+        property="og:image"
+        content={`http${
+          process.env.NEXT_PUBLIC_VERCEL_ENV === "development" ? "" : "s"
+        }://${
+          process.env.NEXT_PUBLIC_VERCEL_URL
+        }/api/og?destination=${destination}`}
+      />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="pt_BR" />
+      <meta property="og:site_name" content="pelegoguincho.com.br"></meta>
+    </>
+  );
+}
