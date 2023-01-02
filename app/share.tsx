325c6756b6ed52f4168b5373aff4bde653f2bb80
Felipe Milani <felmilani@gmail.com>
2023-01-02 16:45:59 -0300

    Change share info

--- app/share.tsx
+++ app/share.tsx
@@ -1,14 +1,14 @@
 "use client";
-export default function Share() {
+export default function Share({ destination }) {
   return (
     <button
       className="bg-slate-500 text-white shadow-md rounded-lg px-4 py-2 text-sm"
       onClick={async () => {
         if (navigator.share) {
           await navigator.share({
-            title: "Titulo aqui",
-            text: "Texto aqui",
-            url: "https://www.pelegoguincho.com.br",
+            title: `Guincho para ${destination}`,
+            text: `Valor do frete de um guincho para ${destination}`,
+            url: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/${destination}`,
           });
         } else {
           console.log("share not supported");
